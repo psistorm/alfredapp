ec413cf59310a298d8d464b09e8e3dbbe59f756c
Bastian Ohmer <github@bastian-ohmer.de>
2014-06-21 16:48:58 +0200

    Added timeout exception handling.

--- Leo Dictionary/source/leo.php
+++ Leo Dictionary/source/leo.php
@@ -18,13 +18,17 @@ class Leo {
 		$cleanedInput = clean_utf8($input);
 		$url = "http://dict.leo.org/".$this->translationCode."/?lang=en&searchLoc=0&search=".urlencode($cleanedInput);
 
-		$str = $w->request($url);
-		$options = $leo->get($str);
+		$str = $w->request($url, array(CURLOPT_CONNECTTIMEOUT => 5, CURLOPT_TIMEOUT => 5));
+		if (!empty($str)) {
+			$options = $leo->get($str);
 		
-		if ($options != array()) {
-			foreach($options as $option) {
-				$w->result(time(), "{".$this->translationCode."}".$option->translatedWord, $option->translatedWord, $option->originalWord, $option->languageCode.".png", "yes", $option->originalWord);
+			if ($options != array()) {
+				foreach($options as $option) {
+					$w->result(time(), "{".$this->translationCode."}".$option->translatedWord, $option->translatedWord, $option->originalWord, $option->languageCode.".png", "yes", $option->originalWord);
+				}
 			}
+		} else {
+			$w->result(time(), "", "Timeout occurred.", $option->originalWord, "icon.png", "no");
 		}
 		return $w->toxml();
 	}
